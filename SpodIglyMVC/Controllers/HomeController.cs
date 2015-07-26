6485c3d97d150028cb06de047ca7f90b9a92cfb5
Queven <andrev369@gmail.com>
2015-07-26 16:10:50 +0200

    26.07.2015

--- SpodIglyMVC/Controllers/HomeController.cs
+++ SpodIglyMVC/Controllers/HomeController.cs
@@ -13,5 +13,9 @@ namespace SpodIglyMVC.Controllers
         {
             return View();
         }
+        public ActionResult Index2()
+        {
+            return View();
+        }
     }
 }
