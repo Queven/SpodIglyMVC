be712cd91be4b544e08286ac87c8458564a0a60b
Queven <andrev369@gmail.com>
2015-07-30 21:51:35 +0200

    Site Map

---
+++ SpodIglyMVC/Infrastructure/ProductDetalisDynamicNodeProvider.cs
@@ -0,0 +1,30 @@
+﻿using MvcSiteMapProvider;
+using SpodIglyMVC.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SpodIglyMVC.Infrastructure
+{
+    public class ProductDetalisDynamicNodeProvider : DynamicNodeProviderBase
+    {
+        private StoreContext db = new StoreContext();
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var reurnValue = new List<DynamicNode>();
+            foreach (var a in db.Albums)
+            {
+                DynamicNode n = new DynamicNode();
+                n.Title = a.AlbumTitle;
+                n.Key = "Album_" + a.AlbumID;
+                n.ParentKey = "Genre_" + a.GenreID;
+                n.RouteValues.Add("id", a.AlbumID);
+                reurnValue.Add(n);
+            }
+            return reurnValue;
+        }
+    }
+}
---
+++ SpodIglyMVC/Infrastructure/ProductListDynamicNodeProvider.cs
@@ -0,0 +1,31 @@
+﻿using MvcSiteMapProvider;
+using SpodIglyMVC.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpodIglyMVC.Infrastructure
+{
+   public class ProductListDynamicNodeProvider : DynamicNodeProviderBase
+    {
+         
+        private StoreContext db = new StoreContext();
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var reurnValue = new List<DynamicNode>();
+            foreach (var a in db.Genres)
+            {
+                DynamicNode n = new DynamicNode();
+                n.Title = a.Name;
+                n.Key = "Genre_" + a.GenreID;
+                //n.ParentKey = "Genre_" + a.GenreID;
+                n.RouteValues.Add("genrename", a.Name);
+                reurnValue.Add(n);
+            }
+            return reurnValue;
+        }
+    }
+}
+
