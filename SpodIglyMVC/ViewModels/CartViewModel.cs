a42fbfccfd7b068c6c723349323d02cc92af6c85
Queven <andrev369@gmail.com>
2015-08-02 23:15:19 +0200

    Cart

---
+++ SpodIglyMVC/ViewModels/CartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SpodIglyMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpodIglyMVC.ViewModels
+{
+    public class CartViewModel
+    {
+        public List<CartItem> CartItems { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
