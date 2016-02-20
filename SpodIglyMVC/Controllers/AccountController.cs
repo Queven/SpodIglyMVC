c0e3a7d6d3148e0e01b1d211d0fd3adb0642e4ac
Queven <andrev369@gmail.com>
2016-02-20 18:10:24 +0100

    Login and Register part 1 (21)

--- SpodIglyMVC/Controllers/AccountController.cs
+++ SpodIglyMVC/Controllers/AccountController.cs
@@ -1,14 +1,47 @@
-﻿using SpodIglyMVC.ViewModels;
+﻿using SpodIglyMVC.Models;
+using SpodIglyMVC.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
 
 namespace SpodIglyMVC.Controllers
 {
     public class AccountController : Controller
     {
+        private ApplicationUserManager _userManager;
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        private ApplicationSignInManager _signInManager;
+
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set { _signInManager = value; }
+        }
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
         // GET: Account
         public ActionResult Login(string returnUrl)
         {
